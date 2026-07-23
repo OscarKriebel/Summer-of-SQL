1d59fad30fa4d884cf878a568d1621af24a51220
Oscar Kriebel <kriebelo@gmail.com>
2026-07-23 16:23:37 +0100

    Started work on Day 7 Dashboard and made color assigner script

---
+++ Day 7/Tableau Hex Color Assigner.py
@@ -0,0 +1,42 @@
+import re
+
+#Takes the hexcodes from a field and assigns it to be its color
+#Must have a non-custom palette alright assigned, such as automatic to the field
+
+file_location = "{{Tableau .twb File Location}}"
+field_name = "{{Name of Hexcode Field}}"
+dashboard = []
+encoding_section = False
+current_color = ""
+current_value = ""
+
+with open(file_location, "r") as preferences:
+    for line in preferences.readlines():
+        #If in the correct color encoding section
+        if encoding_section:
+            if line.strip().startswith("<map"):
+                #Store the format of the color mapping element
+                current_value = line
+            elif line.strip() == '</map>':
+                #Assign the correct color and add back to the dashboard
+                dashboard.append(current_value.split("'")[0] + "'#" + current_color.split("&quot;")[1] + "'" + current_value.split("'")[2])
+                dashboard.append(current_color)
+                dashboard.append(line)
+            else:
+                #Store the hexcode name
+                current_color = line
+        else:
+            #Otherwise add the line back into the dashboard
+            dashboard.append(line)
+
+    #Check if in the encoding section based on the element and the attribute describing the hexcode field
+        if re.search("<encoding.*field='.*Color RGB.*]'.*>", line.strip()) is not None:
+            encoding_section = True
+        #If out of the encoding, stop checking for the color
+        elif encoding_section and line.strip() == "</encoding>":
+            encoding_section = False
+            dashboard.append(line)
+
+#Overwrite dashboard with new color encoding
+with open(file_location, "w") as new_dashboard:
+    new_dashboard.writelines(dashboard)
